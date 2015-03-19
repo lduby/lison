e41042a13731efdb69dec8a311731637c2764178
lmd <laetitia.duby@gmail.com>
2015-03-19 23:50:58 +0100

    Add factory validation to item model tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item do
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:item)).to be_valid
+  end
+
   it 'is invalid without a title' do
     expect(FactoryGirl.build(:item, title: nil)).not_to be_valid
   end
