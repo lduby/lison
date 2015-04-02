dcf64d2bce741c4c9cd594aaffa2952cef535a5f
lmd <laetitia.duby@gmail.com>
2015-04-02 23:41:52 +0200

    Tests for items controller and adds item action views

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+describe ItemsController do
+
+  describe "GET #index" do
+    it "populates an array of items" do
+      item = FactoryGirl.create(:item)
+      get :index
+      assigns(:items).should eq([item])
+    end
+    it "renders the items :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested item to @item" do
+      item = FactoryGirl.create(:item)
+      get :show, id: item
+      assigns(:item).should eq(item)
+    end
+    it "renders the item :show template" do
+      get :show, id: FactoryGirl.create(:item)
+      response.should render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new Item to @item" do
+      get :new
+      expect(assigns(:item)).to be_a_new(Item)
+      # expect(assigns(:business).addresses.first).to be_a_new(Address)
+    end
+    it "renders the item :new template" do
+      get :new
+      response.should render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new item in the database" do
+        expect{
+          post :create, item: FactoryGirl.attributes_for(:item)
+        }.to change(Item,:count).by(1)
+      end
+      it "redirects to the items :index page" do
+        post :create, item: FactoryGirl.attributes_for(:item)
+        response.should redirect_to Item.last
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new item in the database" do
+        expect{
+          post :create, item: FactoryGirl.attributes_for(:invalid_item)
+        }.to_not change(Item,:count)
+      end
+      it "re-renders the item :new template" do
+        post :create, item: FactoryGirl.attributes_for(:invalid_item)
+        response.should render_template :new
+      end
+    end
+  end
+
+end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :item do
-    title "Test item"
+
+  factory :item do |f|
+    f.title { Faker::Lorem.word }
+  end
+
+  factory :invalid_item, parent: :item do |f|
+    f.title nil
   end
 
 end
