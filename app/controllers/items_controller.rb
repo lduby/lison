fb8e00b90254d113965da4f6fa1cb49accd02f11
lmd <laetitia.duby@gmail.com>
2015-04-15 00:05:04 +0200

    Adds update and destroy actions + matching controller tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,32 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:title)
   end
+
+
 
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -64,4 +64,66 @@ describe ItemsController do
     end
   end
 
+  describe 'PUT #update' do
+    before :each do
+      @item = FactoryGirl.create(:item, title: "Test Item")
+    end
+
+    context "with valid attributes" do
+      it "locates the requested @item" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:item)
+        expect(assigns(:item)).to eq(@item)
+      end
+
+      it "changes @item's attributes" do
+        put :update, id: @item,
+          item: FactoryGirl.attributes_for(:item, title: "Updated test item")
+        @item.reload
+        expect(@item.title).to eq("Updated test item")
+      end
+
+      it "redirects to the updated item" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:item)
+        expect(response).to redirect_to @item
+      end
+    end
+
+    context "with invalid attributes" do
+      it "locates the requested @item" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:invalid_item)
+        expect(assigns(:item)).to eq(@item)
+      end
+
+      it "does not change @item's attributes" do
+        put :update, id: @item,
+          item: FactoryGirl.attributes_for(:item, title: nil)
+        @item.reload
+        expect(@item.title).to eq("Test Item")
+      end
+
+      it "re-renders the edit method" do
+        put :update, id: @item, item: FactoryGirl.attributes_for(:invalid_item)
+        expect(response).to render_template :edit
+      end
+    end
+
+  end
+
+  describe 'DELETE destroy' do
+    before :each do
+      @item = FactoryGirl.create(:item)
+    end
+
+    it "deletes the item" do
+      expect{
+        delete :destroy, id: @item
+      }.to change(Item,:count).by(-1)
+    end
+
+    it "redirects to items#index" do
+      delete :destroy, id: @item
+      expect(response).to redirect_to items_url
+    end
+  end
+
 end
